80e2a7c037a70b390e33bac53e21044ee99fa82f
Vy Nguyen <vyn13217@gmail.com>
2025-10-17 00:15:06 -0400

    Lab week 9 added

--- labweek09/src/App.js
+++ labweek09/src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [student, setStudent] = useState({
+    name: "Vy Nguyen",
+    id: "101488823",
+    college: "George Brown College, Toronto",
+    course: "Fullstack Development - I",
+    lab: "React JS Programming Week09 Lab exercise"
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to {student.course}</h1>
+        <h3>{student.lab}</h3>
+        <p>Your Student ID</p>
+        <h4>{student.id}</h4>
+        <p>Your Name</p>
+        <h4>{student.name}</h4>
+        <p>{student.college}</p>
       </header>
     </div>
   );
